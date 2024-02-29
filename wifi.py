00e8216081dd82eff8eb29a12a71705a5924dd30
صخر الورد <161136396+sakhralword@users.noreply.github.com>
2024-02-29 22:43:18 +0300

    Add files via upload

---
+++ wifi.py
@@ -0,0 +1,15 @@
+import os
+os.system('clear')
+name = input ("enter your name: " )
+os.system('clear')
+print (" مرحباً بك في اداة الوحش صخر يا " +name )
+input('enter')
+os.system('clear')
+print(' «1»	wifi')
+print('«2»	gmail ')
+input('enter number:')
+os.system('clear')
+ucybers00 = 1000000000
+while ucybers00 < 9999999999 :
+    ucybers00= ucybers00 + 1
+    print (ucybers00)
